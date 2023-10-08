5f3a75ea3365b66e4afdbc1fe713249ccb00889f
Jake Callcut <jakecallcut123@gmail.com>
2023-10-08 14:56:28 +0100

    writing verification function

--- UniPilot/script.js
+++ UniPilot/script.js
@@ -1,11 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const submit = document.getElementById('submitButton');
+    fetch('data.txt')
+        .then(response => response.text())
+        .then(text => {
+            const lines = text.split('\n');
+        })
+        .catch(error => {
+            console.error('Error loading text file:', error);
+        });
 
+    const submit = document.getElementById('submitButton');
     submit.addEventListener('click', function() {
         const email = document.getElementById('emailInput').value;
         const password = document.getElementById('passwordInput').value;
-        alert("email is " + email + " and password is " + password);
+
+        if (checkInput(email, password)) {
+          alert('logged in')
+        }
+        else {
+          alert('account not found')
+        }
     });
+
+    function checkInput(_email, _password){
+      var emailFound = false;
+      var passwordFound = false;
+
+      for (var i = 0; i < lines.length; i++) {
+        if (_email == lines[i]) {
+          emailFound = true;
+        }
+        if (_password == lines[i]) {
+          passwordFound = true;
+        }
+      }
+
+      if (emailFound && passwordFound) {
+        return true;
+      }
+      return false;
+    }
 
 });

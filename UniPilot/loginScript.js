9960cec57179939e7bea095c7a5616b872fb2b90
Jake Callcut <jakecallcut123@gmail.com>
2023-10-08 15:25:16 +0100

    script not working

--- UniPilot/loginScript.js
+++ UniPilot/loginScript.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    fetch('data.txt')
-        .then(response => response.text())
-        .then(text => {
-            const lines = text.split('\n');
-        })
-        .catch(error => {
-            console.error('Error loading text file:', error);
-        });
+  const users = [
+  { email: 'jakecallcut', password: 'guinness' },
+  { email: 'jakecallcut', password: 'guinness' },
+  { email: 'jakecallcut', password: 'guinness' }
+  ];
 
     const submit = document.getElementById('submitButton');
     submit.addEventListener('click', function() {
         const email = document.getElementById('emailInput').value;
         const password = document.getElementById('passwordInput').value;
 
-        if (checkInput(email, password)) {
-          alert('logged in')
+        if (!checkInput(email, password)) {
+          window.location.replace('index.html');
         }
         else {
-          alert('account not found')
+          alert('account not found: ' + email + ' ' + password);
         }
     });
 
@@ -26,12 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
       var emailFound = false;
       var passwordFound = false;
 
-      for (var i = 0; i < lines.length; i++) {
-        if (_email == lines[i]) {
+      for (var user in users) {
+        if (user.email == _email) {
           emailFound = true;
         }
-        if (_password == lines[i]) {
-          passwordFound = true;
+        if (user.password == _password) {
+          emailFound = true;
         }
       }
 

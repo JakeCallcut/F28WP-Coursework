17bb78da98e14f6fe703976139f746d28aececf0
Jake Callcut <jakecallcut123@gmail.com>
2023-10-09 01:00:34 +0100

    cleaned up input form and added create account button

--- UniPilot/loginScript.js
+++ UniPilot/loginScript.js
@@ -8,18 +8,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const submit = document.getElementById('submitButton');
     submit.addEventListener('click', function() {
-        const email = document.getElementById('emailInput').value;
-        const password = document.getElementById('passwordInput').value;
+      const email = document.getElementById('emailInput').value;
+      const password = document.getElementById('passwordInput').value;
 
-        if (checkInput(email, password)) {
-
-        }
-        else {
-
-        }
+      if (checkInput(email, password)) {
+          window.location.replace('index.html');
+      }
+      else {
+          alert('Username or password incorrect');
+      }
     });
 
-
+    const create = document.getElementById('createButton');
+    create.addEventListener('click', function() {
+      
+    });
 
 
     function checkInput(_email, _password){

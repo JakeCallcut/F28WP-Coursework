ea29585003aa76015b11a84ec722e5c6b005e9cd
Jake Callcut <jakecallcut123@gmail.com>
2023-11-07 20:39:30 +0000

    started work on a way to add class codes

--- UniPilot/indexScript.js
+++ UniPilot/indexScript.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  
+  const class1 = document.getElementById('class1');
+  const class2 = document.getElementById('class2');
+  const class3 = document.getElementById('class3');
+  const class4 = document.getElementById('class4');
+  var noofClasses = 0;
+
+  const addClasses = document.getElementById('addClasses');
+  addClasses.addEventListener('click', function (){
+    const userInput = prompt('Please enter a class code to add');
+
+    if (userInput) {
+      switch (noofClasses) {
+        case 0:
+          localStorage.setItem('class1', userInput);
+          class1.
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
+
+  });
 
 });
